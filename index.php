b0c86d718cc36f2e844baddcabd1cfc98fe8030e
adamawesomeface <adam.stogdill@gmail.com>
2018-11-27 16:17:56 -0500

    debug links

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <nav class="main-navigation" id="mainNavigation">
       <h1 class="logo"><span>Fro-Go</span></h1>
       <?php if($page == 'home'): ?>
-        <a class="login-btn btn" href="/?page=parent">Signin/up</a>
+        <a class="login-btn btn" href="/parent">Signin/up</a>
       <?php else: ?>
         <a href="?page=task-list" class="btn-link"><i class="fas fa-bell"></i></a>
       <?php endif; ?>
@@ -33,6 +33,13 @@
       <div class="copyright">All Rights Reserved &copy; <?php echo date('Y'); ?></div>
       <div class="social"><a href="http://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></div>
     </footer>
+    <ul class="debug-links">
+      <li><a href="/parent">Parent Dashboard</a></li>
+      <li><a href="/task-list">TaskList/Child Dashboard</a></li>
+      <li><a href="/task-listi-manage">TaskList Managment (parent)</a></li>
+      <li><a href="/task-add">Add New Tasks</a></li>
+      <li><a href="/children-add">Add New Child</a></li>
+    </ul>
   </div>
 </body>
 </html>
